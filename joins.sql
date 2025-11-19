e6d0cbf3ff6012241fbb67dd83a0f58e44908c13
Roger <92322066+notLogi@users.noreply.github.com>
2025-11-19 14:13:27 -0500

    Add files via upload

---
+++ joins.sql
@@ -0,0 +1,33 @@
+USE northwind;
+
+-- 1
+SELECT ProductID, ProductName, UnitPrice, categories.CategoryName FROM products
+JOIN categories ON products.CategoryID = categories.CategoryID
+ORDER BY CategoryName, ProductName;
+
+-- 2
+SELECT ProductID, ProductName, UnitPrice, suppliers.CompanyName FROM products
+JOIN suppliers ON products.SupplierID = suppliers.SupplierID
+ORDER BY ProductName;
+
+-- 3
+SELECT ProductID, ProductName, UnitPrice, categories.CategoryName, suppliers.CompanyName FROM products
+JOIN suppliers ON products.SupplierID = suppliers.SupplierID 
+JOIN categories ON products.CategoryID = categories.CategoryID
+ORDER BY ProductName;
+
+-- 4
+SELECT ProductName, categories.CategoryName FROM products
+JOIN categories ON products.CategoryID = categories.CategoryID
+WHERE UnitPrice = (SELECT MAX(UnitPrice) FROM products);
+
+-- 5
+SELECT OrderID, ShipName, ShipAddress, shippers.CompanyName FROM orders
+JOIN shippers ON orders.ShipVia = shippers.ShipperID
+WHERE ShipCountry = "Germany";
+
+-- 6
+SELECT orders.OrderID, ShipName, ShipAddress, products.ProductName FROM orders 
+JOIN `order details` ON orders.OrderID = `order details`.OrderID
+JOIN products ON `order details`.ProductID = products.ProductID 
+WHERE products.ProductName = "Sasquatch Ale";
